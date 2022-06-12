1f2cb7685106a6d4fd5630ee5996762138287b5d
stellamunson <sftamesis13@gmail.com>
2022-06-11 20:17:15 -0700

    commit tree 80f0e796200bcc9ff081fe1231203564887b057d

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+
+//although writing return customerName.toUpperCase() will work,
+// the test requires us to MODIFY the customerName. So that when
+//it runs upperCaseCustomerName() it will change bob to BOB.
+//you have to use customerName as the variable and have it assigned
+//to a code that uppercases it.
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase()
+}
+
+var bestCustomer;
+function setBestCustomer(){
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'some value';
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'another value'
+
+}
